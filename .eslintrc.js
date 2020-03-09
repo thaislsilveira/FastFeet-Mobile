a6c41d1896c44d81d7640dcbb6d4747320517c8a
thailsilveira <thais_lsilveira@hotmail.com>
2020-03-09 17:02:13 -0300

    configurando background

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,4 +35,11 @@ module.exports = {
     ],
     'import/prefer-default-export': 'off',
   },
+  settings: {
+    "import/resolver": {
+      "babel-plugin-root-import": {
+        rootPathSuffix: "src"
+      }
+    }
+  },
 };
--- src/index.js
+++ src/index.js
@@ -1,35 +1,9 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+
+import Routes from './routes';
 
 import './config/ReactotronConfig';
 
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-});
-
 export default function App() {
-  return (
-    <>
-      <View style={styles.body}>
-        <Text style={styles.sectionTitle}>Step One</Text>
-      </View>
-    </>
-  );
+  return <Routes />;
 }
