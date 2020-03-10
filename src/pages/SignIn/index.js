ce9fed96c390c8c4dd9b9546aaa1e188009080a9
thailsilveira <thais_lsilveira@hotmail.com>
2020-03-10 17:24:28 -0300

    Página SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
+import logo from '~/assets/logo.png';
+
+import Button from '~/components/Button';
 import Background from '~/components/Background';
-// import { Container } from './styles';
+
+import { Container, Form, FormInput, SubmitButton } from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            name="id"
+            keyboardType="number-pad"
+            autoCorrect={false}
+            returnKeyType="send"
+            autoCapitalize="none"
+            placeholder="Informe seu ID de Cadastro"
+          />
+
+          <SubmitButton onPress={() => {}}>Entrar no sistema</SubmitButton>
+        </Form>
+      </Container>
     </Background>
   );
 }
