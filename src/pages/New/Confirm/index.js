adbbfd23253540796f2b6aad7da10a2e6f05ef19
thaislsilveira <thais_lsilveira>
2020-03-27 16:34:30 -0300

    Confirmar Entrega 27.03

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,8 +1,69 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { Alert, Text, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+import {
+  Container,
+  Content,
+  ContentTitle,
+  Card,
+  TakePicture,
+  CameraBackground,
+  Camera,
+  ButtonPicture,
+  SubmitButton,
+} from './styles';
+import Background from '~/components/Background';
+import ImageBacground from '~/assets/delivery.png';
 
 export default function Confirm() {
-  return <View />;
+  const camera = useRef(null);
+  const [pictureUri, setPictureUri] = useState('');
+
+  async function takePicture() {
+    if (camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await camera.current.takePictureAsync(options);
+      await setPictureUri(data.uri);
+    }
+  }
+
+  async function handleSubmit() {}
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <ContentTitle>Confirmar Entrega</ContentTitle>
+        <Card>
+          <TakePicture>
+            {pictureUri ? (
+              <CameraBackground>
+                <Image
+                  source={{ uri: pictureUri }}
+                  style={{ height: '100%' }}
+                />
+              </CameraBackground>
+            ) : (
+              <Camera
+                ref={camera}
+                type={Camera.Constants.Type.back}
+                autoFocus={Camera.Constants.AutoFocus.on}
+                flashMode={Camera.Constants.FlashMode.off}
+                captureAudio={false}
+              />
+            )}
+          </TakePicture>
+
+          <ButtonPicture onPress={() => takePicture()}>
+            <Icon name="photo-camera" color="#fff" size={30} />
+          </ButtonPicture>
+          <SubmitButton disabled={!pictureUri} onPress={handleSubmit}>
+            <Text>Enviar</Text>
+          </SubmitButton>
+        </Card>
+      </Content>
+    </Container>
+  );
 }
--- src/pages/New/Confirm/styles.js
+++ src/pages/New/Confirm/styles.js
@@ -3,6 +3,8 @@ import styled from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
 import { RNCamera } from 'react-native-camera';
 
+import Button from '~/components/Button';
+
 export const Container = styled.SafeAreaView`
   flex: 1;
 `;
@@ -12,40 +14,52 @@ export const Content = styled.View`
   top: -90px;
 `;
 
+export const ContentTitle = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 30px;
+`;
+
 export const Card = styled.View`
   background: #fff;
-  padding: 13px 25px 13px 14px;
-  width: 335px;
+  width: 360px;
   border-radius: 4px;
   margin-bottom: 9px;
 `;
 
 export const TakePicture = styled.View`
-  width: 335px;
-  height: 445px;
-  max-height: 445px;
-  min-height: 445px;
-  margin-bottom: 11px;
+  width: 360px;
+  height: 385px;
+`;
+export const CameraBackground = styled.View`
+  width: 100%;
+  border-radius: 4px;
+  margin: 0 auto;
+  position: relative;
+  overflow: hidden;
 `;
 
 export const Camera = styled(RNCamera)`
   width: 100%;
   height: 100%;
-  max-height: 445px;
-  min-height: 445px;
   align-items: center;
 `;
 
-export const Button = styled(RectButton)`
-  width: 80px;
-  height: 80px;
+export const ButtonPicture = styled(RectButton)`
+  background: rgba(0, 0, 0, 0.3);
+  width: 61px;
+  height: 61px;
+  border-radius: 30px;
+  align-items: center;
   justify-content: center;
-  align-items: center;
+  margin-bottom: 33px;
+  position: absolute;
+  bottom: 40px;
+  left: 40%;
 `;
 
-export const ButtonTitle = styled.Text`
-  font-size: 12px;
-  color: #999;
-  margin-top: 5px;
-  text-align: center;
+export const SubmitButton = styled(Button)`
+  margin-top: 10px;
+  background: #7d40e7;
 `;
